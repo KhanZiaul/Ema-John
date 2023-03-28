439f3569f9fa407ac5674e2451cb43b688b0bdc1
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-29 03:58:38 +0600

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header"
+
 function App() {
 
   return (
-    <h1>welcome</h1>
+    <Header></Header>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../assets/images/favicon.ico'
+import './Header.css'
+const Header = () => {
+    return (
+        <nav className='flex justify-between items-center p-5 bg-slate-400'>
+            <img className='w-[60px]' src={logo} alt="" />
+
+            <div className='text-white flex gap-5'>
+                <a href="/order">Order</a>
+                <a href="/Order Review">Order Review</a>
+                <a href="/Manage Inventory">Manage Inventory</a>
+                <a href="/Login">Login</a>
+            </div>    
+        </nav>
+    );
+};
+
+export default Header;
