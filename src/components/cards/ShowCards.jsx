c739da011f23636c59bd58f316498dcc74ebd78f
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-29 04:44:14 +0600

    added price and Manufacturer

--- src/components/cards/ShowCards.jsx
+++ src/components/cards/ShowCards.jsx
@@ -6,7 +6,9 @@ const ShowCards = (props) => {
     return (
         <div className='border-2 border-gray-300 rounded-lg p-6 '>
             <img src={img} alt="" />
-            <h2 className='my-3 text-xl'>{name}</h2>
+            <h2 className='my-3 text-xl font-medium'>{name}</h2>
+            <h3 className='font-medium'>Price : ${price}</h3>
+            <p className='mt-6'>Manufacturer : {seller}</p>
         </div>
     );
 };
