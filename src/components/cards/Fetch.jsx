8f1fb752de3664bd0f215d169585bdd7a2cf3140
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-29 04:40:39 +0600

    added name and image

--- src/components/cards/Fetch.jsx
+++ src/components/cards/Fetch.jsx
@@ -10,14 +10,14 @@ const Cards = () => {
         .then(data => setdatas(data))
     },[])
     return (
-      <div className='showData'>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 m-5'>
+      <div className='showData m-5'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
           {
             datas.map((data) => <ShowCards data={data}></ShowCards>)
           }  
         </div>
         <div>
-
+          <h2 className='text-2xl text-center'>Order Summary</h2>
         </div>
       </div>
     );
--- src/components/cards/ShowCards.jsx
+++ src/components/cards/ShowCards.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const ShowCards = (props) => {
     console.log(props.data)
-    const {name , price , seller , ratings} = props.data ;
+    const {img , name , price , seller , ratings} = props.data ;
     return (
-        <div className='border-2 border-gray-800 p-6 '>
-
+        <div className='border-2 border-gray-300 rounded-lg p-6 '>
+            <img src={img} alt="" />
+            <h2 className='my-3 text-xl'>{name}</h2>
         </div>
     );
 };
