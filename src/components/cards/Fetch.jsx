fa499b75718207c2c1697112c7ba85a660771fd9
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-29 13:28:40 +0600

    added onClick function on Add To Cart

--- src/components/cards/Fetch.jsx
+++ src/components/cards/Fetch.jsx
@@ -9,15 +9,27 @@ const Cards = () => {
         .then(res => res.json())
         .then(data => setdatas(data))
     },[])
+
+    function cartProduct(product){
+
+      console.log(product)
+    }
+
     return (
       <div className='showData m-5'>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
           {
-            datas.map((data) => <ShowCards data={data}></ShowCards>)
+            datas.map((data) => <ShowCards 
+            key={data.id}
+            data={data}
+            cartProduct={cartProduct}
+            >
+            </ShowCards>)
           }  
         </div>
-        <div>
-          <h2 className='text-2xl text-center'>Order Summary</h2>
+        <div className='text-center p-5 bg-orange-400 text-white rounded-lg'>
+          <h2 className='text-2xl font-semibold'>Order Summary</h2>
+          <p className='mt-8 text-left font-medium'>Selected items : </p>
         </div>
       </div>
     );
--- src/components/cards/ShowCards.jsx
+++ src/components/cards/ShowCards.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const ShowCards = (props) => {
-    console.log(props.data)
+
     const { img, name, price, seller, ratings } = props.data;
+
+    const cartProduct = props.cartProduct ;
+
     return (
         <div className='border-2 border-gray-300 rounded-lg p-6 relative'>
             <img src={img} alt="" />
@@ -10,7 +13,7 @@ const ShowCards = (props) => {
             <h3 className='font-medium'>Price : ${price}</h3>
             <p className='mt-6'>Manufacturer : {seller}</p>
             <p className='my-2'>Rating : {ratings} stars</p>
-            <div className='cursor-pointer flex justify-center p-4 gap-4 bg-orange-500 absolute left-0 bottom-0 w-[100%] mt-4 text-white hover:bg-orange-900'>
+            <div onClick={() => cartProduct(props.data)} className='cursor-pointer flex justify-center p-4 gap-4 bg-orange-500 absolute left-0 bottom-0 w-[100%] mt-4 text-white hover:bg-orange-900'>
                 <h3>Add To Cart</h3>
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
