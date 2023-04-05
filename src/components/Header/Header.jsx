23051f98e455bc11f522a31794edb388fc5c8e1c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-06 03:02:50 +0600

    order and order review path added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from '../../assets/images/favicon.ico'
 import './Header.css'
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <nav className='flex justify-between items-center p-5 bg-slate-400'>
+            <Link to='/order'>
             <img className='w-[60px]' src={logo} alt="" />
+            </Link>
 
             <div className='text-white flex gap-5'>
                 <a href="/order">Order</a>
-                <a href="/Order Review">Order Review</a>
+                <a href="/order_review">Order Review</a>
                 <a href="/Manage Inventory">Manage Inventory</a>
                 <a href="/Login">Login</a>
             </div>    
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import FetchData from "./components/FetchData/Fetch"
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./components/Home/Home";
+import OrderReview from "./components/OrderReview/OrderReview";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Home></Home>,
+    children: [
+      {
+        path:'order',
+        element:<FetchData></FetchData>
+      },
+      {
+        path:'order_review',
+        element:<OrderReview></OrderReview>
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+       <RouterProvider router={router} />
   </React.StrictMode>,
 )
