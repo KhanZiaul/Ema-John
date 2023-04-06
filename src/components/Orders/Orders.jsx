290d7336c4a32d5c1f912c5c65f6a77e72ce7f21
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-06 17:17:53 +0600

    ReviewItems component done

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
-import { TrashIcon } from '@heroicons/react/24/solid'
+import ReviewItems from '../ReviewItems/ReviewItems';
 
 const Orders = () => {
     const products = useLoaderData()
@@ -10,28 +10,11 @@ const Orders = () => {
         <div className='grid grid-cols-1 gap-20 md:grid-cols-2 md:px-16 my-12'>
             <div>
                 {
-                    products.map(product => {
-                        return (
-                            <div key={product.id} className='md:w-[571px] md:h-[107px] border-2 border-gray-300 rounded-lg mb-5'>
-                                <div className='flex flex-col md:flex-row md:items-center justify-between px-2 gap-5'>
-                                    <div className='flex gap-3 items-center mt-2'>
-                                        <div>
-                                        <img className='w-[91px] h-[91px]' src={product.img} alt="" />
-                                        </div>
-                                        <div>
-                                            <p className='font-semibold'>{product.name}</p>
-                                            <p>Price : <span className='text-yellow-500 font-medium'>${product.price}</span></p>
-                                            <p>Quantity : <span className='text-yellow-500 font-medium'>{product.quantity}</span></p>
-                                        </div>
-                                    </div>
-
-                                    <div className='md:bg-red-200 p-3 rounded-full cursor-pointer hover:bg-red-300 flex justify-center items-center'>
-                                        <TrashIcon className="h-6 w-6 text-red-500 " />
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })
+                    products.map(product => <ReviewItems
+                        key={product.id}
+                        product={product}
+                    >
+                    </ReviewItems>)
                 }
             </div>
             <div>
---
+++ src/components/ReviewItems/ReviewItems.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { TrashIcon } from '@heroicons/react/24/solid'
+
+const ReviewItems = ({product}) => {
+    return (
+        <>
+            <div className='md:w-[571px] md:h-[107px] border-2 border-gray-300 rounded-lg mb-5'>
+                <div className='flex flex-col md:flex-row md:items-center justify-between px-2 gap-5'>
+                    <div className='flex gap-3 items-center mt-2'>
+                        <div>
+                            <img className='w-[91px] h-[91px]' src={product.img} alt="" />
+                        </div>
+                        <div>
+                            <p className='font-semibold'>{product.name}</p>
+                            <p>Price : <span className='text-yellow-500 font-medium'>${product.price}</span></p>
+                            <p>Quantity : <span className='text-yellow-500 font-medium'>{product.quantity}</span></p>
+                        </div>
+                    </div>
+
+                    <div className='md:bg-red-200 p-3 rounded-full cursor-pointer hover:bg-red-300 flex justify-center items-center'>
+                        <TrashIcon className="h-6 w-6 text-red-500 " />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ReviewItems;
