73d82e1cde525066077047c00d10f037e92d8121
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-06 15:24:14 +0600

    getData from dataBase and set quantity  done

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,8 +1,21 @@
+import { getShoppingCart } from "../utilities/fakedb"
+
 const Loader =  async() =>{
 
     const res = await fetch('products.json')
     const datas = await res.json()
-    return datas
+    const getDataFromDatabase = getShoppingCart()
+    console.log(getDataFromDatabase);
+    let saveCart = [];
+    for(let id in getDataFromDatabase){
+        let matchProduct = datas.find(data => data.id === id)
+        if(matchProduct){
+            let quantity = getDataFromDatabase[id]
+            matchProduct.quantity = quantity
+            saveCart.push(matchProduct)
+        }
+    }
+    return saveCart;
 }
 
 export default Loader;
--- src/components/utilities/fakedb.js
+++ src/components/utilities/fakedb.js
@@ -2,27 +2,16 @@
 const addToDb = id => {
 
     let shoppingCart = getShoppingCart();
-
     // add quantity
-
     const quantity = shoppingCart[id];
-
     if (!quantity) {
-
         shoppingCart[id] = 1;
-
     }
-
     else {
-
         const newQuantity = quantity + 1;
-
         shoppingCart[id] = newQuantity;
-
     }
-
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
-    
 }
 
 
