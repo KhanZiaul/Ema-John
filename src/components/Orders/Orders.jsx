95c20e0745ae132ef285142b9fee24647123c808
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-06 15:30:46 +0600

    show orderSummary in Orders component done

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -4,15 +4,14 @@ import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
     const products = useLoaderData()
-    console.log(products)
-
+    
     return (
         <div className='grid grid-cols-1 gap-20 md:grid-cols-2 md:px-16 md:py-12'>
           <div>
             <h2>Order list</h2>
           </div>
           <div>
-            <Cart carts={[]}></Cart>
+            <Cart carts={products}></Cart>
           </div>
         </div>
     );
