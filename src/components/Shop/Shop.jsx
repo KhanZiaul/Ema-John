29a4af8206d68eee058edb48feb830ad35c045f8
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-06 21:31:17 +0600

    updated Shop component

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,77 +2,75 @@ import React, { useEffect, useState } from 'react';
 import Cards from '../Cards/Cards';
 import './Shop.css'
 import Cart from '../Cart/Cart';
-import { addToDb, getShoppingCart } from '../utilities/fakedb';
+import { addToDb, deleteShoppingCart, getShoppingCart } from '../utilities/fakedb';
 
 const Shop = () => {
-    const [datas,setdatas] = useState([]);
-    const [carts,setCarts] = useState([]);
+  const [datas, setdatas] = useState([]);
+  const [carts, setCarts] = useState([]);
 
-    useEffect(()=>{
-        fetch('products.json')
-        .then(res => res.json())
-        .then(data => setdatas(data))
-    },[])
+  useEffect(() => {
+    fetch('products.json')
+      .then(res => res.json())
+      .then(data => setdatas(data))
+  }, [])
 
-    useEffect(()=>{
-      let savedCarts = [] ;
-     // console.log(datas)
-      let getCarts = getShoppingCart() ;
-      // console.log(getCarts)
-      for(let id in getCarts){
-        // console.log(cart)
-        let matchProduct = datas.find((product) => product.id === id) ;
-        // console.log(matchProduct)
-        if(matchProduct){
-          let newQuantity = getCarts[id];
-          matchProduct.quantity = newQuantity ;
-          savedCarts.push(matchProduct) ;
-        }
+  useEffect(() => {
+    let savedCarts = [];
+    let getCarts = getShoppingCart();
+    for (let id in getCarts) {
+      let matchProduct = datas.find((product) => product.id === id);
+      if (matchProduct) {
+        let newQuantity = getCarts[id];
+        matchProduct.quantity = newQuantity;
+        savedCarts.push(matchProduct);
       }
-      // console.log('saveCart', savedCarts)
-      setCarts(savedCarts);
-    },[datas]) 
-
-
-    function cartProduct(product){
-      // const newProducts = [...carts,product] ;
-
-      let newProducts = [] ;
-
-      const exists = carts.find(pd => pd.id === product.id) ;
-
-      if(!exists){
-        product.quantity = 1 ;
-        newProducts = [...carts , product] 
-      }
-      else{
-        exists.quantity = exists.quantity + 1;
-
-        const remaining = carts.filter(pd => exists.id !== pd.id) ;
-
-        newProducts = [...remaining , exists]
-      }
-      setCarts(newProducts) ;
-      addToDb(product.id)
     }
+    setCarts(savedCarts);
+  }, [datas])
 
-    return (
-      <div className='showData m-5 flex flex-col-reverse md:flex-row'>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-          {
-            datas.map((data) => <Cards 
+
+  function cartProduct(product) {
+    // const newProducts = [...carts,product] ;
+    let newProducts = [];
+    const exists = carts.find(pd => pd.id === product.id);
+    if (!exists) {
+      product.quantity = 1;
+      newProducts = [...carts, product]
+    }
+    else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = carts.filter(pd => exists.id !== pd.id);
+      newProducts = [...remaining, exists]
+    }
+    setCarts(newProducts);
+    addToDb(product.id)
+  }
+
+  function deleteAllItems() {
+    setCarts([])
+    deleteShoppingCart()
+  }
+
+  return (
+    <div className='showData m-5 flex flex-col-reverse md:flex-row'>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+        {
+          datas.map((data) => <Cards
             key={data.id}
             data={data}
             cartProduct={cartProduct}
-            >
-            </Cards>)
-          }  
-        </div>
-        <div>
-          <Cart carts ={carts}></Cart>
-        </div>
+          >
+          </Cards>)
+        }
       </div>
-    );
+      <div>
+        <Cart
+          carts={carts}
+          deleteAllItems={deleteAllItems}
+        ></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
